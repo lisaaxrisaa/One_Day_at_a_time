87937a23996082c72b1d74b787fa460c1490bae9
Lisa Fujita <lisafujita90@gmail.com>
2024-08-07 19:53:28 -0400

    coding class #3

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
   const d = new Date();
   document.getElementById('date-demo').textContent = d.toDateString();
 });
+
+function changeMood() {
+  const img = document.getElementById('excited-button');
+  const whiteImg = 'img/excited-outline.svg';
+  const blackImg = 'img/excited-filled.svg';
+  if (img.src.endsWith(whiteImg)) {
+    img.src = blackImg;
+    img.alt = 'Excited face filled';
+  } else {
+    img.src = whiteImg;
+    img.alt = 'Excited Face outline';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('mood-button');
+  button.addEventListener('click', changeMood);
+});
